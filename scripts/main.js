f35bcdeb6aae938267fa833dca4e19838caadf76
hoffmantim <hoffmant@hilbertk12.org>
2017-12-06 10:36:25 -0600

    First commit of test website for learning front end basics

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'BJJ White Belt Test';
+
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
