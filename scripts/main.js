183f76746997372c63a8a54340278ef791687b21
hoffmantim <hoffmant@hilbertk12.org>
2017-12-06 11:17:58 -0600

    Attempt to add Change User button and welcome script

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,33 @@ var myHeading = document.querySelector('h1');
 myHeading.textContent = 'BJJ White Belt Test';
 
 
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/BJJ_White_Belt.png') {
+      myImage.setAttribute ('src','images/MMA-Icon-BJJ.png');
+    } else {
+      myImage.setAttribute ('src','images/BJJ_White_Belt.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.', 'My name');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'BJJ is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'BJJ is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
 }
